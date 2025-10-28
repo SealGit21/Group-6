63f6c01efca8263739a683b5fa4f76263a826c6b
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-10-28 15:13:11 +0700

    Header + tạo Home, Products, Cart
    Home sẽ để hiển thị sơ các loại sản phẩm, Product mới là nơi hiển thị chính

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import { Navbar, Nav, Container, FormControl, Form, Button, Image } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" style={{ height: '80px' }}>
+            <Container className="d-flex justify-content-between align-items-center">
+                <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
+                    <Image
+                        src="/logo/thiet-ke-logo-shop-giay-19_1584095087.jpg"
+                        alt="Logo"
+                        width="40"
+                        height="40"
+                        roundedCircle
+                        className="me-2"
+                    />
+                    ShoeShop
+                </Navbar.Brand>
+                <Nav className="mx-auto" >
+                    <NavLink to="/" className="nav-link px-3" style={({ isActive }) => ({
+                        color: isActive ? 'red' : 'white', textDecoration: 'none', fontSize: '1.4rem',
+                    })}>Home</NavLink>
+
+                    <NavLink to="/products" className="nav-link px-3" style={({ isActive }) => ({
+                        color: isActive ? 'red' : 'white', textDecoration: 'none', fontSize: '1.4rem'
+                    })}>Products</NavLink>
+                </Nav>
+
+                <Form className="d-flex">
+                    <FormControl className="me-2" type="text" placeholder="Search" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
+                <NavLink to="/cart" className=" ms-4" style={({ isActive }) => ({
+                    color: isActive ? 'red' : 'white', textDecoration: 'none', fontSize: '1.4rem'
+                })}><Image
+                        src="/logo/cart.png"
+                        alt="Cart"
+                        width="35"
+                        height="35"
+                        className="me-1"
+                    />
+
+                </NavLink>s
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
