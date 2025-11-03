c7994bca3d5e34dc8438b7d1ebe597d7b7c24dab
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-11-04 00:44:53 +0700

    Sửa cấu trúc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cart" element={<Register />} />
+            
           </Routes>
         </div>
         <Footer />
