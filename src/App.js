5339bb9199b3df59c3177b8e44af65c3fce82c5a
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-11-06 16:24:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './components/Header';
 import AdminHeader from './components/AdminHeader';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Detail from './pages/Detail'
-import Products from './pages/Products'
 import Login from './pages/Login';
+import Detail from './pages/Detail';
+import Products from './pages/Products';
 import Register from './pages/Register';
 import AdminDashboard from './pages/AdminDashboard';
 import Profile from './pages/Profile';
 
-function AppContent() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
-
-  return (
-    <>
-      {isAdminRoute ? <AdminHeader /> : <Header />}
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<AdminDashboard/>} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
-      </div>
-    </>
-  );
-}
+import { CartProvider } from './components/CartContext';
 
 function App() {
   return (
     <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <div className="container mt-4 flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products/:id" element={<Detail />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            
-          </Routes>
+      <CartProvider>
+        <div className="d-flex flex-column min-vh-100">
+
+          <Header />
+
+          <div className="container mt-4 flex-grow-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products/:id" element={<Detail />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+
+              <Route path="/admin" element={<AdminDashboard />} />
+            </Routes>
+          </div>
+
+          <Footer />
+
         </div>
-        <Footer />
-      </div>
-      <AppContent />
+      </CartProvider>
     </Router>
   );
 }
